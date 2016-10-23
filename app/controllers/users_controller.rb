11aa48f8dda50e857b41e2eefdf94e576e660509
Frank Sommers <ssommers21@gmail.com>
2016-10-22 17:50:15 -0700

    adds redirect to home when a user logs in, adds high contrast background colors in appcss (will change back)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,9 @@ post '/users' do
 
     if @user.save
       session[:id] = @user.id
-      redirect "/users/#{@user.id}"
+      redirect '/'
+      # redirects to users profile page
+      # redirect "/users/#{@user.id}"
     else
       @errors = @user.errors.full_messages
       erb :'users/new'
